eb85fd2b977511ca3b78eec719d07bf3bf67344b
Gijs Cox <1918893cox@zuyd.nl>
2023-10-27 12:07:09 +0200

    Add project files.

---
+++ Duurzame-Consumentkeuzes/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Duurzame_Consumentkeuzes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Duurzame-Consumentkeuzes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Duurzame_Consumentkeuzes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
