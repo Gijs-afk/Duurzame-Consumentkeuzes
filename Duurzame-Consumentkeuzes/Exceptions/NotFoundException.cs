e32a03a0555a5d13b7c8b46802832bd1127498d5
Gijs Cox <1918893cox@zuyd.nl>
2023-10-27 12:15:42 +0200

    Custom errors

---
+++ Duurzame-Consumentkeuzes/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Duurzame_Consumentkeuzes.Exceptions;
+
+    public class NotFoundException: Exception
+    {
+        public NotFoundException(string msg) : base(msg) { }
+    }
+
---
+++ Duurzame-Consumentkeuzes/Exceptions/NotImplementedException.cs
@@ -0,0 +1,5 @@
+﻿namespace Duurzame_Consumentkeuzes.Exceptions; 
+public class NotImplementedException : Exception
+{
+    public NotImplementedException(string msg) :base(msg) { }
+}
